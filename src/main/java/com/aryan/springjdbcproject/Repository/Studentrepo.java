bc6b9583b3f16ba7b27d56dfe52aaa41d6fbf71d
Aryan Sohani <aryan.112123@gmail.com>
2026-03-17 07:59:43 +0530

    JdbcTemplate

--- src/main/java/com/aryan/springjdbcproject/Repository/Studentrepo.java
+++ src/main/java/com/aryan/springjdbcproject/Repository/Studentrepo.java
@@ -1,6 +1,8 @@
 package com.aryan.springjdbcproject.Repository;
 
 import com.aryan.springjdbcproject.Model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,9 +11,23 @@ import java.util.List;
 @Repository
 public class Studentrepo {
 
+
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     public void save(Student s)
     {
-        System.out.println(s+"is saved");
+        String query="INSERT INTO student(RollNo,Name,marks) VALUES (?,?,?)";
+        int row = jdbc.update(query,s.getRollNo(),s.getName(),s.getName());
+        System.out.println(row + "affected");
     }
     public List<Student> findall()
     {
